69a2a11585368e67f77ed9d33fb4be18fb7baf58
Michael Karamuth <michael@arsero.com>
2022-01-17 21:52:25 +0400

    Add helper fn for emailing invitations

--- getin/settings.py
+++ getin/settings.py
@@ -1,3 +1,24 @@
 from django.conf import settings
+from django.contrib.sites.models import Site
 
 CODE_BYTES = getattr(settings, "GETIN_CODE_BYTES", 8)
+
+SITE_ID = getattr(settings, "SITE_ID", None)
+
+if not SITE_ID:
+    SITE_ID = getattr(settings, "GETIN_SITE_ID", None)
+
+
+default_subject = "You've been invited"
+
+if SITE_ID:
+    try:
+        site = Site.objects.get(pk=SITE_ID)
+    except Site.DoesNotExist:
+        site = None
+
+    if site:
+        default_subject += f"to {site.name}"
+
+
+EMAIL_SUBJECT = getattr(settings, "GETIN_EMAIL_SUBJECT", default_subject)
---
+++ getin/utils.py
@@ -0,0 +1,30 @@
+import logging
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.contrib.sites.models import Site
+from getin import settings as app_settings
+
+log = logging.getLogger(__name__)
+
+
+def email_invitation(code, **kwargs):
+    email = kwargs.pop("email")
+
+    try:
+        site = Site.objects.get(pk=app_settings.SITE_ID)
+    except Site.DoesNotExist:
+        site = None
+    ctx = {"code": code, "site": site}
+    message = render_to_string("getin/invitation_email.txt", ctx)
+    html_message = render_to_string("getin/invitation_email.html", ctx)
+
+    send_mail(
+        subject=app_settings.EMAIL_SUBJECT,
+        message=message,
+        from_email=None,
+        recipient_list=[
+            email,
+        ],
+        html_message=html_message,
+    )
+    log.info(f"Invitation emailed to {email}")
