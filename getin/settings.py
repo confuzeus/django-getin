5fd85fdb54e7575b98a692b17d79a763669facc7
Michael Karamuth <michael@arsero.com>
2022-01-17 23:15:19 +0400

    Expect that Django sites is configured

--- getin/settings.py
+++ getin/settings.py
@@ -1,24 +1,27 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
+from django.core.exceptions import ImproperlyConfigured
 
 CODE_BYTES = getattr(settings, "GETIN_CODE_BYTES", 8)
+
+default_subject = "You've been invited"
 
 SITE_ID = getattr(settings, "SITE_ID", None)
 
 if not SITE_ID:
     SITE_ID = getattr(settings, "GETIN_SITE_ID", None)
 
+if not SITE_ID:
+    raise ImproperlyConfigured("Please set SITE_ID or configure django.contrib.sites.")
 
-default_subject = "You've been invited"
 
-if SITE_ID:
-    try:
-        site = Site.objects.get(pk=SITE_ID)
-    except Site.DoesNotExist:
-        site = None
+try:
+    site = Site.objects.get(pk=SITE_ID)
+except Site.DoesNotExist:
+    raise ValueError(f"Couldn't find a site with id {SITE_ID}.")
 
-    if site:
-        default_subject += f"to {site.name}"
+
+default_subject += f"to {site.name}"
 
 
 EMAIL_SUBJECT = getattr(settings, "GETIN_EMAIL_SUBJECT", default_subject)
