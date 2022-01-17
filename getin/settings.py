81d4bd8fbd2d15067b8d7c3aba40cba86cba6975
Michael Karamuth <michael@arsero.com>
2022-01-17 23:58:02 +0400

    Simplify default email subject line

--- getin/settings.py
+++ getin/settings.py
@@ -1,10 +1,9 @@
 from django.conf import settings
-from django.contrib.sites.models import Site
 from django.core.exceptions import ImproperlyConfigured
 
 CODE_BYTES = getattr(settings, "GETIN_CODE_BYTES", 8)
 
-default_subject = "You've been invited"
+default_subject = "Your invitation"
 
 SITE_ID = getattr(settings, "SITE_ID", None)
 
@@ -13,15 +12,5 @@ if not SITE_ID:
 
 if not SITE_ID:
     raise ImproperlyConfigured("Please set SITE_ID or configure django.contrib.sites.")
-
-
-try:
-    site = Site.objects.get(pk=SITE_ID)
-except Site.DoesNotExist:
-    raise ValueError(f"Couldn't find a site with id {SITE_ID}.")
-
-
-default_subject += f"to {site.name}"
-
 
 EMAIL_SUBJECT = getattr(settings, "GETIN_EMAIL_SUBJECT", default_subject)
