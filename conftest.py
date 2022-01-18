8b0c47eb7ef010b1fff9333ebb8d497f1726f360
Michael Karamuth <michael@arsero.com>
2022-01-18 12:09:51 +0400

    Add useful fixtures for testing invitations

--- conftest.py
+++ conftest.py
@@ -1,8 +1,13 @@
+from unittest.mock import MagicMock
+
 import pytest
 from faker import Faker
 from django.contrib.sites.models import Site
+from django.contrib.auth import get_user_model
 
 from getin.models import Invitation
+
+User = get_user_model()
 
 
 @pytest.fixture
@@ -23,3 +28,28 @@ def site(db) -> Site:
 @pytest.fixture
 def invite_code():
     return Invitation.generate_code()
+
+
+@pytest.fixture
+def unsent_invitation(db):
+    return Invitation.create()
+
+
+@pytest.fixture
+def sent_invitation(db, unsent_invitation):
+    send_fn = MagicMock()
+    unsent_invitation.send_invitation(send_fn)
+    unsent_invitation.save()
+    return unsent_invitation
+
+
+@pytest.fixture
+def consumed_invitation(db, sent_invitation, user):
+    sent_invitation.consume(user)
+    sent_invitation.save()
+    return sent_invitation
+
+
+@pytest.fixture
+def user(db, fake):
+    return User.objects.create(username=fake.user_name(), email=fake.ascii_email())
