e5a5adebe8acea1bafc16583058caa7b068256f4
Michael Karamuth <michael@arsero.com>
2022-01-28 13:01:46 +0400

    Test TransitionNotAllowed from admin

--- getin/tests/test_admin.py
+++ getin/tests/test_admin.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock
+
 import pytest
 
 from getin.admin import _transition_queryset
@@ -26,3 +28,15 @@ def test_transition_expire(admin_request, invitation_admin, sent_invitation):
 
     for invitation in Invitation.objects.all():
         assert invitation.state == InvitationState.EXPIRED.value
+
+
+@pytest.mark.django_db
+def test_transition_not_allowed(admin_request, invitation_admin, unsent_invitation):
+    qs = Invitation.objects.all()
+
+    transition_method = InvitationState.EXPIRED
+
+    mock_message_user = MagicMock()
+    invitation_admin.message_user = mock_message_user
+    _transition_queryset(invitation_admin, admin_request, qs, transition_method)
+    mock_message_user.assert_called()
