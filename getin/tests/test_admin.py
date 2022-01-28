983c45a465a595275d7c621839098f769b5094ed
Michael Karamuth <michael@arsero.com>
2022-01-28 13:04:26 +0400

    Test mark invitation as sent from admin

--- getin/tests/test_admin.py
+++ getin/tests/test_admin.py
@@ -26,7 +26,7 @@ def test_transition_expire(admin_request, invitation_admin, sent_invitation):
     transition_method = InvitationState.EXPIRED
     _transition_queryset(invitation_admin, admin_request, qs, transition_method)
 
-    for invitation in Invitation.objects.all():
+    for invitation in qs:
         assert invitation.state == InvitationState.EXPIRED.value
 
 
@@ -40,3 +40,14 @@ def test_transition_not_allowed(admin_request, invitation_admin, unsent_invitati
     invitation_admin.message_user = mock_message_user
     _transition_queryset(invitation_admin, admin_request, qs, transition_method)
     mock_message_user.assert_called()
+
+
+@pytest.mark.django_db
+def test_transition_mark_sent(admin_request, invitation_admin, unsent_invitation):
+    qs = Invitation.objects.all()
+
+    transition_method = InvitationState.SENT
+    _transition_queryset(invitation_admin, admin_request, qs, transition_method)
+
+    for invitation in qs:
+        assert invitation.state == InvitationState.SENT.value
