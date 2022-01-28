bdc37eb467c143287a8826bba2491b34ab7ad9f2
Michael Karamuth <michael@arsero.com>
2022-01-28 12:56:18 +0400

    Test expire invitation from admin

--- getin/tests/test_admin.py
+++ getin/tests/test_admin.py
@@ -5,13 +5,24 @@ from getin.models import Invitation, InvitationState
 
 
 @pytest.mark.django_db
-def test_transition_force_expire(admin_user, invitation_admin, unsent_invitation, rf):
+def test_transition_force_expire(admin_request, invitation_admin, unsent_invitation):
     qs = Invitation.objects.all()
 
     transition_method = InvitationState.EXPIRED
-    request = rf.get("/")
-    request.user = admin_user
-    _transition_queryset(invitation_admin, request, qs, transition_method, force=True)
+    _transition_queryset(
+        invitation_admin, admin_request, qs, transition_method, force=True
+    )
+
+    for invitation in Invitation.objects.all():
+        assert invitation.state == InvitationState.EXPIRED.value
+
+
+@pytest.mark.django_db
+def test_transition_expire(admin_request, invitation_admin, sent_invitation):
+    qs = Invitation.objects.all()
+
+    transition_method = InvitationState.EXPIRED
+    _transition_queryset(invitation_admin, admin_request, qs, transition_method)
 
     for invitation in Invitation.objects.all():
         assert invitation.state == InvitationState.EXPIRED.value
